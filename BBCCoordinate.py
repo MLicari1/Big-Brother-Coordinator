1f375fbccf7ea41fa448287a15549305fbb93c04
Nate <14845347+garlic-os@users.noreply.github.com>
2024-09-19 14:06:33 -0500

    Refactor

--- BBCCoordinate.py
+++ BBCCoordinate.py
@@ -1,34 +1,45 @@
-def read_stuff(filename):
+def read_picks(filename: str) -> dict[str, list[str]]:
+    """
+    key: individual
+    value: his picks
+    """
     with open(filename, 'r') as file:
-        data = list(map(lambda x: x.split(','), file.read().split('\n')))
-        return {row[0]: row[1:] for row in data}
-        
+        rows: list[list[str]] = []
+        for line in file:
+            rows.append(line.strip().replace(", ", ",").split(","))
+        return {row[0]: row[1:] for row in rows}
 
-bigs = read_stuff(r"Results - Bigs.csv")
-littles = read_stuff(r"Results - Littles.csv")
-in_eachothers_list = []
-one_on_ones = []
 
-littles_paired = set()
-bigs_paired = set()
+bigs = read_picks("Responses from Bigs.csv")
+littles = read_picks("Responses from Littles.csv")
+
+# big, little, pairing score (0 is best)
+imperfect_pairings: list[tuple[str, str, int]] = []
+
+# big-little pairings where each was the other's number one
+perfect_pairings: list[tuple[str, str]] = []
+
+# names of individuals who have been processed
+littles_paired: set[str] = set()
+bigs_paired: set[str] = set()
 
 for big, big_picks in bigs.items():
-    for big_rating, little in enumerate(big_picks):
-        if big in (little_picks := littles[little]) and little in big_picks:
-            little_rating = little_picks.index(big)
-            score = big_rating + little_rating
+    for big_rank, little in enumerate(big_picks):
+        little_picks = littles[little]
+        if big in little_picks and little in big_picks:
+            little_rank = little_picks.index(big)
+            score = big_rank + little_rank
             if score == 0:
-                one_on_ones.append((big, little))
+                perfect_pairings.append((big, little))
             else:
-                in_eachothers_list.append((big, little, score))
-            
+                imperfect_pairings.append((big, little, score))
+
             bigs_paired.add(big)
             littles_paired.add(little)
 
 
 
 unpaired_littles = {little for little in littles if little not in littles_paired}
-
 unpaired_bigs = {big for big in bigs if big not in bigs_paired}
 
 loose_pairings = set()
@@ -36,31 +47,34 @@ loose_pairings = set()
 
 for big in unpaired_bigs:
     for little in unpaired_littles:
-        if big in littles[little] or little in bigs[big]:
+        big_picks = bigs[big]
+        little_picks = littles[little]
+        if little in big_picks or big in little_picks:
             loose_pairings.add((big, little))
 
-homeless_littles = unpaired_littles - {l for _, l in loose_pairings}
+orphaned_littles = unpaired_littles - {little for _, little in loose_pairings}
 
 
-    
-in_eachothers_list.sort(key=lambda x: x[2])
+
+imperfect_pairings.sort(key=lambda x: x[2])
+
 print(
 """
 clarification: lower score is better than a higher score...
 score is calculated by adding up "preference" position within each others list
-"preference" position simply means big choice number + little choice number (-2 since coding is base 0)
+"preference" position simply means big choice number + little choice number (-2 since coding is index starts at 0)
 This an arbitrary ranking. The decision to make pairs is still up to the Kappa and BBC's discretion. Use this tool as a recommendation only.
 if a big or little puts a pick down multiple times, the code will still run and execute but will give some meaningless/ duplicate results
 """
 )
 
-print("One-to-One")
-for big, little in one_on_ones:
+print("Perfect pairs - each other's number one picks")
+for big, little in perfect_pairings:
     print("\t", big, "-", little)
 
 
 print("\n\nhad each other on list")
-for big, little, score in in_eachothers_list:
+for big, little, score in imperfect_pairings:
     print("\t", score, big, "-", little)
 
 print("\n\nunpaired littles - littles with no list matches", *unpaired_littles, sep="\n\t")
@@ -71,5 +85,5 @@ for big, little in loose_pairings:
     print("\t", big, '-', little)
 
 print("\n\nFinal Stand - these littles were no-one's top five :(", "\n\t")
-print(*homeless_littles, sep="\n\t")
+print(*orphaned_littles, sep="\n\t")
 
